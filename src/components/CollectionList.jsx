05729e3e8a2d4e19e1cd9e856aceef69b7155919
Lain4504 <tienhuynhn2004@gmail.com>
2024-09-08 18:43:18 +0700

    create dropdown menu

--- src/components/CollectionList.jsx
+++ src/components/CollectionList.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const CollectionList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default CollectionList
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  return (
+    <div className='relative'>
+      <button
+        onClick={toggleDropdown}
+        className='flex flex-col items-center gap-1 text-gray-900'
+      >
+        SẢN PHẨM
+      </button>
+      {/* Dropdown menu */}
+      {isOpen && (
+        <div className='absolute z-10 mt-2 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-lg'>
+          <div className='py-1'>
+            <a href="#" className='block px-4 py-2 text-sm text-black hover:bg-gray-100'>Toàn bộ danh mục</a>
+          </div>
+          <ul className='py-2 text-sm text-black'>
+            <li>
+              <a href="#" className='block px-4 py-2 hover:bg-gray-100'>Dashboard</a>
+            </li>
+            <li>
+              <a href="#" className='block px-4 py-2 hover:bg-gray-100'>Settings</a>
+            </li>
+            <li>
+              <a href="#" className='block px-4 py-2 hover:bg-gray-100'>Earnings</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CollectionList;
